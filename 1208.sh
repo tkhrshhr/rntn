123f059f2206afb8d3a7cb63c4c8f90a9b9d01eb
tkhrshhr <tkhrshhr@gmail.com>
2017-12-08 00:34:21 +0900

    add 1208.sh

--- 1208.sh
+++ 1208.sh
@@ -4,7 +4,7 @@ for weight in 0.0001 0.0005 0.0007 0.0009; do
     echo $weight
     python train.py -r 1 -m $model -w $weight -d 25 > log/$model$weight &
   done;
-  for p in 1, 2, 3, 10; do
+  for p in 1 2 3 10; do
     echo rs
     echo $weight
     python train.py -r 1 -m rs -w $weight -d 25 -p $p -q $p > log/rs$p$weight &
