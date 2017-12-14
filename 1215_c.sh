f9abba928bb07a1c6155f9a5e506108754893c67
tkhrshhr <tkhrshhr@gmail.com>
2017-12-15 02:28:43 +0900

    add 1215_c

---
+++ 1215_c.sh
@@ -0,0 +1,12 @@
+for d in 10; do
+  echo $d
+  for m in rn rt rd rc; do
+    echo $m
+    python train.py -r 1 -m $m -d $d -p 1 -q 1 -e 1 > log/1215_m$m\d$d &
+  done
+  m=rs
+  for p in 1 2 4; do
+    echo $m
+    python train.py -r 1 -m $m -d $d -p $p -q $p -e 1 > log/1215_m$m\d$d\p$p &
+  done
+done
